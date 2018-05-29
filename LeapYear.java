b7751edbe9bec83bd9bbb99c094c0c5b1b09e6c2
kingofahn <kingofahn@gmail.com>
2018-05-29 19:14:26 +0900

    윤년 수정

--- LeapYear.java
+++ LeapYear.java
@@ -5,12 +5,21 @@ public class LeapYear{
 		System.out.println("년도 입력해주세요");
 		int year = sc.nextInt();
 		String result ="";
-		if(year%4==0){
-		result="윤년";
-		} else {
+		if(year%4!=0 && year%400!=0){
 		result="평년";
+		} else {
+		result="윤년";
 		}
 		System.out.print(result);
-
+		
 	}
 }
+
+/*
+연도를 4로 나눈값이 0 이라면 윤년일 수 있다.  
+그러나 해당 연수가 100으로 나누어 떨어지면 평년이다. 
+2000년은 4로 나눈값이 0 이라서 윤년일 수 있는데.. 
+다시 이 값이 100으로 나눠 떨어지면 평년이다. 
+평년이라 해도 다시 400으로 나눠 떨어지는 연도는 윤년이다. 
+예시) 2000년 과 2016 년을 입력하면 윤년으로 나옴
+*/
