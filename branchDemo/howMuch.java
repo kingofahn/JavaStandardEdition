987d993552bb1d42471e5970e91288a637ec6b97
kingofahn <kingofahn@gmail.com>
2018-05-29 15:55:18 +0900

    branch폴더 추가

--- branchDemo/howMuch.java
+++ branchDemo/howMuch.java
@@ -2,23 +2,19 @@ import java.util.Scanner;
 public class howMuch{
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
-
 		System.out.println("얼마에요?");
 		int price = sc.nextInt();
 		System.out.println("몇개 드릴까요");
 		int unit = sc.nextInt();
 		System.out.println(unit +"개 주세요");
-		System.out.println("총 금액은" + price*unit + "원 입니다.");
+		System.out.println("총 금액은 " + price*unit + " 원 입니다.");
 		System.out.println("비싸요 깍아주세요!!!");
 		System.out.println("몇 퍼센트 dc 할까요?");
 		int dc = sc.nextInt();
-		String result = "";
-
+		String result = price*unit*(100-dc)/100+"원" ;
 		if(dc>=10){
 			result ="안팔아요!!!";
-		} else{
-			result = price*unit*(100-dc)/100 + "원 입니다.";
-		}
+		} 
 		System.out.println(result);
 	}
 }
